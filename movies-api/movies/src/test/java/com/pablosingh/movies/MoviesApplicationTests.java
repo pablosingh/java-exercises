dc7636a1cd0ee08fe694f0b1a2a5f6cc7b367f23
pablosingh <pablo22_4@hotmail.com>
2024-09-25 21:02:24 -0300

    config inicial de spring

---
+++ movies-api/movies/src/test/java/com/pablosingh/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablosingh.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
